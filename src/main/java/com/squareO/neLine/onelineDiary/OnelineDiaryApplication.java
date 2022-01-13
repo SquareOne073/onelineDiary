9a077f7346d67efd62f144597725548a7f35ce04
이승원 <squareo073@gmail.com>
2022-01-14 08:54:41 +0900

    commit

---
+++ src/main/java/com/squareO/neLine/onelineDiary/OnelineDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.squareO.neLine.onelineDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnelineDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnelineDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squareO/neLine/onelineDiary/OnelineDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squareO.neLine.onelineDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnelineDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
