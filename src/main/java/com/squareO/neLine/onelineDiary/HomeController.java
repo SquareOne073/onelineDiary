89245e65c8b0f27a765c29ae69c068dde8b1b48a
이승원 <squareo073@gmail.com>
2022-01-16 13:04:41 +0900

    commit

--- src/main/java/com/squareO/neLine/onelineDiary/HomeController.java
+++ src/main/java/com/squareO/neLine/onelineDiary/HomeController.java
@@ -1,4 +1,4 @@
-package com.squareO.neLine;
+package com.squareO.neLine.onelineDiary;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
